f5ac5f75133d40555717b8e8a063395144a4011c
Stanislav Dmitriev <dmistas@mail.ru>
2021-01-17 18:12:33 +0500

    fixed 2d task

--- task_2.php
+++ task_2.php
@@ -37,21 +37,25 @@
 
                     <div class="fs-lg fw-300 p-5 bg-white border-faded rounded mb-g">
                         <?php
-//                      в верстке былди разные заголовки h2 и h3 я принял это как ошибку и сделал все с h2
-//                      если нужно учитывать именно разный уровень заголовков в массиве могу переделать
                         $articles = [
-                            'Privacy' => 'Your privacy is important to us at SmartAdmin and the protection, confidentiality and integrity of your personal data are our prime concerns.
+                            [
+                                'title' => 'Privacy',
+                                'text' => 'Your privacy is important to us at SmartAdmin and the protection, confidentiality and integrity of your personal data are our prime concerns.
                                 We will only use your personal information to administer your account, provide the products and services you have requested from us, and to keep you informed about our products and services (if you have consented to this).
                                 We only use your data for the purposes for which it was collected and, where relevant, to meet local legal obligations.
-                                We will retain your personal information only for as long as is necessary for the purposes for which the information was collected, or as long as is required pursuant to law.',
-                            'Cookies and other similar technologies' => 'We collect certain data through cookies and similar technologies (e.g. web beacons, tags, device identifiers). Cookies are text files placed on your computer to collect standard internet log information and visitor behaviour information. This information is used to track visitor use of the website and to compile statistical reports on website activity. We register your interaction with our services in order to improve our website, content and services. Our use of such technologies and the data collected is described in more detail in our Cookie Policy. You can manage your cookie preferences through your browser settings.'
+                                We will retain your personal information only for as long as is necessary for the purposes for which the information was collected, or as long as is required pursuant to law.'
+                            ],
+                            [
+                                'title' => 'Cookies and other similar technologies',
+                                'text' => 'We collect certain data through cookies and similar technologies (e.g. web beacons, tags, device identifiers). Cookies are text files placed on your computer to collect standard internet log information and visitor behaviour information. This information is used to track visitor use of the website and to compile statistical reports on website activity. We register your interaction with our services in order to improve our website, content and services. Our use of such technologies and the data collected is described in more detail in our Cookie Policy. You can manage your cookie preferences through your browser settings.',
+                            ],
                         ];
-                        foreach ($articles as $header => $article):
-                        ?>
-                        <h2><?= $header?></h2>
-                        <p class="mb-g">
-                            <?=$article?>
-                        </p>
+                        foreach ($articles as $article):
+                            ?>
+                            <h2><?= $article['title'] ?></h2>
+                            <p class="mb-g">
+                                <?= $article['text'] ?>
+                            </p>
                         <?php
                         endforeach;
                         ?>
